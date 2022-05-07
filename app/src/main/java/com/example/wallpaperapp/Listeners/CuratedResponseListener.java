4590f654cb7e8cad6755e3ecfea53a6dcf00f428
Shameer24 <mohamedshameer2406@gmail.com>
2022-05-07 08:19:15 +0530

    hhh

--- app/src/main/java/com/example/wallpaperapp/Listeners/CuratedResponseListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.wallpaperapp.Listeners;
-
-import com.example.wallpaperapp.Models.CuratedApiResponse;
-
-public interface CuratedResponseListener {
-    void onFetch(CuratedApiResponse response,String message);
-    void onError(String message);
-}
